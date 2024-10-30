c2ed25706519204324a5603efe11eb79d10e3cf9
Supremekirb <99102603+Supremekirb@users.noreply.github.com>
2024-10-31 09:01:31 +1030

    Desktop entry autocreation for wayland

--- ebme.py
+++ ebme.py
@@ -6,7 +6,8 @@ import logging
 import os
 import sys
 
-from PySide6.QtCore import QFile, QSettings
+from PySide6.QtCore import QFile, QSettings, QStandardPaths
+from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication
 
 import resources_rc
@@ -15,7 +16,6 @@ import src.misc.debug as debug
 import src.misc.icons as icons
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -28,6 +28,7 @@ if __name__ == "__main__":
                         help="Immediately load a project at this path",
                         type=str,
                         default=None)
+    parser.add_argument("--no-autoload", action="store_true", help="Don't automatically load the most recent project even if the setting is enabled.")
     parser.add_argument("--system-output", action="store_true", help="Use system output instead of the debug window")
     
     args = parser.parse_args()
@@ -50,7 +51,39 @@ if __name__ == "__main__":
     # this means we dont have to specify this every time we use QSettings
     app.setApplicationName("EBME")
     app.setOrganizationName("PK Hack")
-
+    
+    # do desktop file stuff if on wayland
+    if QApplication.platformName() == "wayland":
+        # check if a desktop file exists in the application folders
+        # I'm pretty sure this is how Qt itself does it... hard to find it in the source code
+        appPaths = QStandardPaths.standardLocations(QStandardPaths.StandardLocation.ApplicationsLocation)
+        if not QStandardPaths.locate(QStandardPaths.StandardLocation.ApplicationsLocation, "EBME.desktop"):
+            logging.info(f"Hi Wayland friend, creating a desktop entry now at {appPaths[0]}/EBME.desktop")
+            logging.info("Also copying icon to ~/.local/share/icons/EBMEIcon.png")
+            
+            # ico files don't seem to work too well with auto-matching in desktop entries,
+            # so just get the 128x128 one. It's not the end of the world
+            QIcon(":/logos/icon.ico").pixmap(128, 128).save(os.path.join(
+                QStandardPaths.standardLocations(QStandardPaths.StandardLocation.HomeLocation)[0],
+                ".local/share/icons/EBMEIcon.png"
+            ))
+            
+            file = QFile(":/misc/EBME.desktop")
+            file.open(QFile.OpenModeFlag.ReadOnly | QFile.OpenModeFlag.Text)
+            desktopFile = file.readAll().data().decode()
+            if sys.argv[0] == __file__: # running from python source so desktop file exec needs to be different
+                logging.warning("Seems like we're creating the desktop file while running from source.")
+                logging.warning("The desktop file's Exec fields will use the binary of this Python. You may wish to change it to your own startup script or to an EBME binary.")
+                desktopFile = desktopFile.replace("&EXEC", f"{sys.executable} {sys.argv[0]}")
+            else: # otherwise binary, so sys.argv[0] gives us the location we need. Easy!
+                desktopFile = desktopFile.replace("&EXEC", sys.argv[0])
+                
+            with open(os.path.join(appPaths[0],
+                                   "EBME.desktop"), "w") as file:
+                file.write(desktopFile)
+        
+        QApplication.setDesktopFileName("EBME")
+            
     # create the main window
     # we do this now and not later so we can force a repaint after applying the theme with setUpdatesEnabled
     # ...surely there's a better way to do this?
@@ -73,9 +106,10 @@ if __name__ == "__main__":
     MainWin.setUpdatesEnabled(True)
 
     MainWin.show()
-
+    
     # If a path was passed, load it
-    # If no path was passed and settings say to do so, load the last successfully loaded project.
+    # If no path was passed and we're told not to autoload, boot without loading
+    # Otherwise, if the settings say to, load the last successfully loaded project.
     # Otherwise, nothing
     if args.path:
         logging.info(f"Path was passed, loading project at {args.path}")
@@ -83,7 +117,7 @@ if __name__ == "__main__":
     else:
         loadLast = settings.value("main/disableLoadLast", type=bool)
         lastPath = settings.value("main/LastProjectPath", type=str)
-        if lastPath and not loadLast:
+        if lastPath and not loadLast and not args.no_autoload:
             logging.info(f"The last loaded project was at {lastPath}")
             MainWin.projectWin.openDirectory(lastPath)
 
